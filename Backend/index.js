507e7383f196a7e31b6be6a3c0b90e5818564c80
jakejansen <jansenjake599@gmail.com>
2023-03-08 13:08:18 +0200

    simple mispelled word redone

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin", "http://localhost:8080");
     res.header("Access-Control-Allow-Headers", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    request.header("Access-Control-Allow-Credentials", "true");
+    req.header("Access-Control-Allow-Credentials", "true");
     next();
 });
 app.use(route);
